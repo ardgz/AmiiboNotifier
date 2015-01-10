4bbdd0a8c99675435cda3c55cf7d843adc43463b
Adan Rodriguez <mario2485@yahoo.com>
2015-01-10 05:22:11 -0600

    Added targetInventory module and initial program done

--- AmiiboNotifier.py
+++ AmiiboNotifier.py
@@ -1,5 +1,11 @@
 ﻿from Tkinter import *
 import os
+import sys
+import mandrill
+import Queue
+import threading
+import time
+import targetInventory
 
 class Amiibo:
 	def __init__(self, name):
@@ -11,6 +17,15 @@ class Amiibo:
 		return self.var
 	def getVarValue(self):
 		return self.var.get()
+		
+class TextRedirector(object):
+	def __init__(self, widget, tag="stdout"):
+		self.widget = widget
+		self.tag = tag
+	def write(self, str):
+		self.widget.configure(state="normal")
+		self.widget.insert("end", str, (self.tag,))
+		self.widget.configure(state="disabled")
 	
 class Application(Frame):
 	def __init__(self, master=None):
@@ -18,13 +33,15 @@ class Application(Frame):
 		self.grid()
 		self.master.title("Amiibo Notifier")
 		self.buildInterface(self.master)
-		
+		sys.stdout = TextRedirector(self.outputBox, "stdout")
+		sys.stderr = TextRedirector(self.outputBox, "stderr")
+		self.processRunning = False
 	
 	def buildInterface(self, master):
 		self.amiiboList = self.setVariables()
-		
 		self.buildLabelFrames(master)
 		self.buildInput(master)
+		self.buildOutput(master)
 		
 	def buildLabelFrames(self, master):
 		i = 0
@@ -32,17 +49,17 @@ class Application(Frame):
 		currentColumn = 0
 		
 		waveOneLabelFrame = LabelFrame(master, text='Wave 1', padx=5, pady=5)
-		waveOneLabelFrame.grid()
+		waveOneLabelFrame.grid(sticky=W+E, padx=5)
 		self.populateFrame(waveOneLabelFrame, i, self.numWaveOne)
 		i += self.numWaveOne
 		
 		waveTwoLabelFrame = LabelFrame(master, text='Wave 2', padx=5, pady=5)
-		waveTwoLabelFrame.grid()
+		waveTwoLabelFrame.grid(sticky=W+E, padx=5)
 		self.populateFrame(waveTwoLabelFrame, i, self.numWaveTwo)
 		i += self.numWaveTwo
 		
 		waveThreeLabelFrame = LabelFrame(master, text='Wave 3', padx=5, pady=5)
-		waveThreeLabelFrame.grid()
+		waveThreeLabelFrame.grid(sticky=W+E, padx=5)
 		self.populateFrame(waveThreeLabelFrame, i, self.numWaveThree)
 		
 	def populateFrame(self, labelFrame, i, waveSize):
@@ -82,25 +99,186 @@ class Application(Frame):
 		return allAmiibos
 	
 	def buildInput(self, master):
-		inputLabelFrame = LabelFrame(master, text='Gmail notification', padx=5, pady=5)
-		inputLabelFrame.grid()
+		# Create Input Label Frame
+		inputLabelFrame = LabelFrame(master, text='Email notification', padx=5, pady=5)
+		inputLabelFrame.grid(sticky=W+E, padx=5)
 		
-		emailLabel = Label(inputLabelFrame, text='Gmail address:', justify=LEFT, anchor=W, padx=5)
+		# Create Email Label
+		emailLabel = Label(inputLabelFrame, text='Email:', justify=LEFT, anchor=W, padx=5)
 		emailLabel.grid(row=0, column=0, sticky=W)
-		Entry(inputLabelFrame).grid(row=0, column=1)
 		
-		passwordLabel = Label(inputLabelFrame, text='Password:', justify=LEFT, anchor=W, padx=5)
-		passwordLabel.grid(row=1, column=0, sticky=W)
-		Entry(inputLabelFrame, show='*').grid(row=1, column=1)
+		# Create Email Field
+		self.email = Entry(inputLabelFrame, width='33')
+		self.email.grid(row=0, column=1)
 		
-		b = Button(master, text='Run Amiibo Notifier', width=30, command=self.testFunction)
-		b.grid()
+		# Create Zip Code Frame
+		zipCodeFrame = LabelFrame(master, text='Area Search', padx=5, pady=5)
+		zipCodeFrame.grid(sticky=W+E, padx=5)
 		
-	def testFunction(self):
-		os.system('cls' if os.name == 'nt' else 'clear')
-		for amiibo in self.amiiboList:
-			print amiibo.getName(), amiibo.getVarValue()
+		# Create Zip Code Label
+		zipLabel = Label(zipCodeFrame, text='Zip Code:', justify=LEFT, anchor=W, padx=5)
+		zipLabel.grid(row=0, column=0, sticky=W)
+		
+		# Create Zip Code Field
+		self.zip = Entry(zipCodeFrame, width='6')
+		self.zip.grid(row=0, column=1)
+		
+		# Create Button Frame
+		buttonFrame = Frame(master, padx=5, pady=5)
+		buttonFrame.grid(sticky=W+E)
+		
+		# Create Buttons
+		self.amiiboButton = Button(buttonFrame, text='Run Amiibo Notifier', width=25, command=self.amiiboNotifierFunction) #command = self.testFunction
+		self.amiiboButton.grid(row=0, padx=10)
+		self.stopButton = Button(buttonFrame, text='Stop', width=6, command=self.stopFunction)
+		self.stopButton.grid(row=0, column=1, padx=20)
+		
+	def buildOutput(self, master):
+		self.outputBox = Text(master, wrap='word', state='disabled', width=40)
+		self.outputBox.grid(row=1, column=1, rowspan=5, pady=10, padx=10)
+		
+	def emailTask(self, stop_event):
+		while(not stop_event.is_set()):
+			# Make outputBox editable and clear contents
+			self.outputBox.config(state='normal')
+			self.outputBox.delete("1.0", END)
 			
+			# Disable button for now
+			self.amiiboButton.config(state='disabled')
+			
+			# Check if required correct data is given
+			completeDataFlag = self.validateData()
+			if not completeDataFlag:
+				self.amiiboButton.config(state='normal')
+				self.processRunning = False
+				return
+			
+			# Process data
+			toEmailAddress = self.email.get()
+			fromEmailAddress = 'amiiboalert@gmail.com'
+			fromName = 'Amiibo Alert'
+			emailSubject = 'Amiibo Notification'
+			username = toEmailAddress.partition('@')[0]
+			zip_code = self.zip.get()
+			email_body = ''
+			selectedAmiiboNameList = []
+			for amiibo in self.amiiboList:
+				if amiibo.getVarValue() == 1:
+					selectedAmiiboNameList.append(amiibo.getName())		
+			print 'Checking Inventory...'
+			
+			# Initialize message data and add found Amiibo data if any were found in specified area
+			email_body = ''		
+			email_body += targetInventory.targetInventory(selectedAmiiboNameList, zip_code)	
+			
+			# open config file to fetch Mandrill API KEY
+			config_file = open('config', 'r')  
+			API_KEY = config_file.readline().rstrip()
+			
+			# Send email if message data was successfully generated
+			if email_body != '':
+				print 'Sending email...'
+				try:
+					mandrill_client = mandrill.Mandrill(API_KEY)
+					message = { 
+								'from_name': fromName,
+								'from_email': fromEmailAddress,
+								'to':[{'email': toEmailAddress}],
+								'subject': emailSubject,
+								'text': email_body,
+								'important': True,
+								'track_opens': True,
+								'track_clicks': True
+							}
+					result = mandrill_client.messages.send(message=message)
+					
+					recipientEmail = result[0]['email']
+					# the sending status of the recipient - either "sent", "queued", "scheduled", "rejected", or "invalid"
+					status = result[0]['status']
+					rejectReason = result[0]['reject_reason']
+					
+					if status == 'sent':
+						print 'Successfully sent email to', recipientEmail
+					elif status == 'rejected':
+						print 'Email rejected, please try another email address'
+					elif status == 'invalid':
+						print 'Email is invalid, please provide correct email'
+				
+				except mandrill.Error, e:
+					print 'A mandrill error occurred: %s - %s' % (e.__class__, e)
+					# A mandrill error occurred: <class 'mandrill.UnknownSubaccountError'> - No subaccount exists with the id 'customer-123'
+					raise
+			else:
+				print 'Selected amiibos not found in your area.'
+			
+			MINUTES = 15  	# Can be changed to a lower amount, but not recommended
+							# to avoid overwhelming websites
+			SECONDS_IN_MINUTES = 60 # Constant, DO NOT CHANGE
+			timeInterval = SECONDS_IN_MINUTES * MINUTES # DO NOT CHANGE
+			
+			print 'Searching again in', MINUTES, 'minutes.'
+			stop_event.wait(timeInterval)
+		
+		# Mark process as finished
+		self.processRunning = False
+		
+		# Enable button
+		self.amiiboButton.config(state='normal')
+			
+		# Make outputBox noneditable
+		self.outputBox.config(state='disabled')
+	
+	def validateData(self):
+		completeDataFlag = True
+		
+		# Check if required data is given
+		selectionCount = 0
+		for amiibo in self.amiiboList:
+			if amiibo.getVarValue() == 1:
+				selectionCount += 1
+		
+		# Check if any amiibos were selected before proceeding
+		if selectionCount == 0:
+			print 'No amiibos selected'
+			completeDataFlag = False
+			
+		# Check if email was entered and valid
+		if len(self.email.get()) == 0:
+			print 'Email not specified'
+			completeDataFlag = False
+		elif '@' not in self.email.get():
+			print 'Email is not valid'
+			completeDataFlag = False
+		elif '.' not in self.email.get():
+			print 'Email is not valid'
+			completeDataFlag = False
+		
+		# Check if zip code is valid
+		if len(self.zip.get()) == 0:
+			print 'Zipcode not provided'
+			completeDataFlag = False
+		elif len(self.zip.get().strip()) != 5:
+			print 'Zipcode must have only five digits'
+			completeDataFlag = False
+		elif not self.zip.get().strip().isdigit():
+			print 'Zipcode must consist of only digits'
+			completeDataFlag = False
+			
+		return completeDataFlag
+		
+	def amiiboNotifierFunction(self):
+		self.processRunning = True
+		self.process_stop = threading.Event()
+		self.process = threading.Thread(target=self.emailTask, args=(self.process_stop,))
+		self.process.start()
+		
+	def stopFunction(self):
+		if self.processRunning:
+			self.process_stop.set()
+			print 'Program stopped'
+			self.amiiboButton.config(state='normal')
+			self.outputBox.config(state='disabled')
+		
 if __name__ == '__main__':
 	root = Tk()
 	app = Application(root)
---
+++ targetInventory.py
@@ -0,0 +1,171 @@
+﻿from bs4 import BeautifulSoup
+import urllib
+import urllib2
+import sys
+import re
+import time
+import os
+import sendgrid
+
+def connectAndRetrieveData(zipStr, dcpiStr):
+	url = 'http://brickseek.com/target-inventory-checker'
+	data = {
+		'zip': zipStr,
+		'dcpi': dcpiStr
+	}
+	req = urllib2.Request(url, 
+						  data = urllib.urlencode(data)
+						  )	
+	try:
+		response = urllib2.urlopen(req)
+	except urllib2.HTTPError, e:
+		print e.code
+	except urllib2.URLError, e:
+		print e.args	
+	else:
+		html = response.read()
+		soup = BeautifulSoup(html)
+		table_code = soup.table
+		
+		tags_list = []
+		
+		for item in table_code:
+			if len(item.contents) != 1:
+				tags_list.append(str(item))
+				
+		strings_list = []
+				
+		for tag in tags_list:
+			aString = re.sub(r'<tr><td style=""><b>', "", tag)
+			aString = re.sub(r'</b><br>', " | ", aString)
+			aString = re.sub(r'<br>', " | ", aString)
+			aString = re.sub(r'<br/>', " | ", aString)
+			aString = re.sub(r'</br></br></td><td><b>', " | ", aString)
+			aString = re.sub(r'</b><a style="color:red">|</b><a style="color:green">', " ", aString)
+			aString = re.sub(r'</a> \| <b>', " | ", aString)
+			aString = re.sub(r'</b>', "", aString)
+			aString = re.sub(r'</td></tr>', "", aString)
+			strings_list.append(aString)
+		
+		targetStoreList = []
+		
+		for string in strings_list:
+			storeDataList = string.split(' | ')
+			targetStoreList.append(storeDataList)
+			
+		email_body = analyzeStoreStock(targetStoreList, dcpiStr)
+		return email_body
+		
+def analyzeStoreStock(targetStoreList, dcpiStr):
+	email_body = ""
+	for storeData in targetStoreList:
+		if "Yes" in storeData[4]:
+			amiibo_name = getAmiiboName(dcpiStr)
+			email_body = prepareEmailStoreData(storeData, amiibo_name, email_body)
+	
+	return email_body
+
+def getAmiiboName(dcpiStr):
+	if dcpiStr == '207-00-5001':
+		return 'Mario'
+	elif dcpiStr == '207-00-5002':
+		return 'Pikachu'
+	elif dcpiStr == '207-00-5003':
+		return 'Link'
+	elif dcpiStr == '207-00-5004':
+		return 'Peach'
+	elif dcpiStr == '207-00-5005':
+		return 'Yoshi'
+	elif dcpiStr == '207-00-5006':
+		return 'Donkey Kong'
+	elif dcpiStr == '207-00-5007':
+		return 'Kirby'
+	elif dcpiStr == '207-00-5008':
+		return 'Villager'
+	elif dcpiStr == '207-00-5009':
+		return 'Fox'
+	elif dcpiStr == '207-00-5010':
+		return 'Samus'
+	elif dcpiStr == '207-00-5011':
+		return 'Marth'
+	elif dcpiStr == '207-00-5012':
+		return 'Wii Fit Trainer'
+	elif dcpiStr == '207-00-5013':
+		return 'Luigi'
+	elif dcpiStr == '207-00-5014':
+		return 'Zelda'
+	elif dcpiStr == '207-00-5015':
+		return 'Little Mac'
+	elif dcpiStr == '207-00-5016':
+		return 'Diddy Kong'
+	elif dcpiStr == '207-00-5017':
+		return 'Pit'
+	elif dcpiStr == '207-00-5018':
+		return 'Captain Falcon'
+
+def getAmiiboDCPI(amiiboName):
+	if amiiboName == 'Mario':
+		return '207-00-5001'
+	if amiiboName == 'Pikachu':
+		return '207-00-5002'
+	if amiiboName == 'Link':
+		return '207-00-5003'
+	if amiiboName == 'Peach':
+		return '207-00-5004'
+	if amiiboName == 'Yoshi':
+		return '207-00-5005'
+	if amiiboName == 'Donkey Kong':
+		return '207-00-5006'
+	if amiiboName == 'Kirby':
+		return '207-00-5007'
+	if amiiboName == 'Villager':
+		return '207-00-5008'
+	if amiiboName == 'Fox':
+		return '207-00-5009'
+	if amiiboName == 'Samus':
+		return '207-00-5010'
+	if amiiboName == 'Marth':
+		return '207-00-5011'
+	if amiiboName == 'Wii Fit Trainer':
+		return '207-00-5012'
+	if amiiboName == 'Luigi':
+		return '207-00-5013'
+	if amiiboName == 'Zelda':
+		return '207-00-5014'
+	if amiiboName == 'Little Mac':
+		return '207-00-5015'
+	if amiiboName == 'Diddy Kong':
+		return '207-00-5016'
+	if amiiboName == 'Pit':
+		return '207-00-5017'
+	if amiiboName == 'Captain Falcon':
+		return '207-00-5018'
+
+def prepareEmailStoreData(targetStore, amiibo_name, email_body):
+	email_body += "AMIIBO: " + amiibo_name + '\n'
+	email_body +=  "Store Name: " + targetStore[0] + '\n'
+	email_body +=  "Address: " + targetStore[1] + '\n'
+	email_body += "Phone Number: " + targetStore[2] + '\n'
+	email_body += "Distance: " + targetStore[3] + '\n'
+	email_body += targetStore[4] + '\n'
+	email_body += targetStore[5] + '\n'
+	email_body += "==================================\n"
+	return email_body
+
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+# ======================================================
+# ======================================================
+
+def targetInventory(amiiboNameList, ZIP_CODE):
+	dcpi_list = []
+	for amiiboName in amiiboNameList:
+		amiibo_dcpi = getAmiiboDCPI(amiiboName)
+		dcpi_list.append(amiibo_dcpi)
+	
+	email_body = ''
+	for dcpi in dcpi_list:
+		email_body += connectAndRetrieveData(ZIP_CODE, dcpi)
+	
+	return email_body
